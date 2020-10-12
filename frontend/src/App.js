dca2d530af74f8ba6bf883864c8d18304e02f96b
Marcela Arcos Caballero <a01703191@itesm.mx>
2020-10-12 08:30:58 -0500

    Cambios nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 import RegistrarAsesoria from './Components/RegistrarAsesoria';
+import navbarNA from './Components/navbarNA';
 import { NavbarBrand } from 'reactstrap';
 
 
@@ -10,6 +12,7 @@ import { NavbarBrand } from 'reactstrap';
 function App() {
   return (
     <div className="App">
+    <navbarNA/>
     <RegistrarAsesoria/>
     </div>
   );
--- frontend/src/Components/navbarNA.js
+++
@@ -1,8 +0,0 @@
-var React = require ('react');
-const { setGlobalCssModule } = require('reactstrap/lib/utils');
-var navbarNA = React.createClass({
-
-
-})
-
-setGlobalCssModule.exports=navbarNa;
