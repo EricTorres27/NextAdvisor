7c66e4ba94eb58653e8acc6ad033e0c294a26d48
EricTorres27 <Ilovethisgame17*>
2020-10-12 14:33:17 -0500

    Se agregaron los componentes basicos de la aplicacion

---
+++ frontend/src/ThemeConfig.js
@@ -0,0 +1,13 @@
+import {createMuiTheme} from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#386FA4',
+            dark: '#133C55',
+            light:'#59A5D8'
+        }
+    }
+})
+
+export default theme;
