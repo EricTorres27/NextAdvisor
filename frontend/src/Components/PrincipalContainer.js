c9097e2d8be8a71f47ba1b08af4b2d82d9830194
heyestefi <51332106+heyestefi@users.noreply.github.com>
2020-11-29 16:40:12 -0600

    Fixes

--- frontend/src/Components/PrincipalContainer.js
+++ frontend/src/Components/PrincipalContainer.js
@@ -195,7 +195,7 @@ const PrincipalContainer = () => {
                         <Route exact path="/Materias" component={subjects} />
                         <Route exact path="/EditarPerfil" component={EditarPerfil} />
                         <Route exact path="/ConsultarPerfil" component={ConsultarPerfil} />
-                        <Route exact path="/MisAsesorias" component={ConsultarPerfil} />
+                        <Route exact path="/MisAsesorias" component={MisAsesorias} />
                     </div>
                 </div>
             </Router>
